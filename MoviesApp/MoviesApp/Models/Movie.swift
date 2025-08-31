112f436b39d37168c427ded9c3e36b000501c661
Pablo Caraballo Gómez <pcaraballogomez@gmail.com>
2025-08-31 12:56:29 +0200

    Add AddReviewScreen

--- MoviesApp/MoviesApp/Models/Movie.swift
+++ MoviesApp/MoviesApp/Models/Movie.swift
@@ -13,7 +13,7 @@ final class Movie {
     var title: String
     var year: Int
     @Relationship(deleteRule: .cascade, inverse: \Review.movie)
-    var reviews: [Review]? = []
+    var reviews: [Review] = []
 
     init(title: String, year: Int) {
         self.title = title
---
+++ MoviesApp/MoviesApp/Screens/AddReviewScreen.swift
@@ -0,0 +1,57 @@
+//
+//  AddReviewScreen.swift
+//  MoviesApp
+//
+//  Created by Pablo Caraballo Gómez on 31/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct AddReviewScreen: View {
+
+    let movie: Movie
+
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var context
+    @State private var subject: String = ""
+    @State private var description: String = ""
+
+    private var isFormValid: Bool {
+        !subject.isEmptyOrWhitespace && !description.isEmptyOrWhitespace
+    }
+
+    var body: some View {
+        Form {
+            TextField("Subject", text: $subject)
+            TextField("Body", text: $description)
+        }
+        .navigationTitle("Add Review")
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button("Close") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+
+                    let review = Review(subject: subject,
+                                        body: description,
+                                        movie: movie)
+                    context.insert(review)
+
+                    do {
+                        try context.save()
+                        movie.reviews.append(review)
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+
+                    dismiss()
+
+                }.disabled(!isFormValid)
+            }
+        }
+    }
+}
